e4c91996b3b710e36b54946f2844032bb8a7b28a
Paul2708 <paul.hoger@gmx.net>
2019-02-26 10:06:25 +0100

    Added red color prefix for op players

--- claim/src/main/java/de/paul2708/claim/scoreboard/ScoreboardManager.java
+++ claim/src/main/java/de/paul2708/claim/scoreboard/ScoreboardManager.java
@@ -2,11 +2,9 @@ package de.paul2708.claim.scoreboard;
 
 import de.paul2708.claim.model.ClaimInformation;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import org.bukkit.scoreboard.DisplaySlot;
-import org.bukkit.scoreboard.Objective;
-import org.bukkit.scoreboard.RenderType;
-import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.*;
 
 /**
  * This singleton class manages the used scoreboards.
@@ -20,25 +18,50 @@ public final class ScoreboardManager {
     private Scoreboard scoreboard;
     private Objective objective;
 
+    private Team adminTeam;
+
     /**
      * Create the scoreboard.
      */
     public void createScoreboard() {
-        this.scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
-        this.objective = scoreboard.registerNewObjective("chunks", "dummy", "", RenderType.INTEGER);
+        this.scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
 
+        // Clear objective and teams
+        this.objective = scoreboard.getObjective("chunks");
+        if (objective != null) {
+            objective.unregister();
+        }
+
+        this.adminTeam = scoreboard.getTeam("0-admin");
+        if (adminTeam != null) {
+            adminTeam.unregister();
+        }
+
+        // Create object and teams
+        this.objective = scoreboard.registerNewObjective("chunks", "dummy", "Chunks", RenderType.INTEGER);
         objective.setDisplaySlot(DisplaySlot.PLAYER_LIST);
+
+        this.adminTeam = scoreboard.registerNewTeam("0-admin");
+        adminTeam.setColor(ChatColor.DARK_RED);
     }
 
     /**
-     * Update the score for a player.
+     * Update the scoreboard for a player. It will be set for everyone.
      *
      * @param player player
      */
     public void update(Player player) {
         objective.getScore(player.getName()).setScore(ClaimInformation.get(player.getUniqueId()).getChunks().size());
 
-        player.setScoreboard(scoreboard);
+        if (player.isOp()) {
+            if (adminTeam.hasEntry(player.getName())) {
+                adminTeam.removeEntry(player.getName());
+            }
+
+            adminTeam.addEntry(player.getName());
+        }
+
+        Bukkit.getOnlinePlayers().forEach(p -> p.setScoreboard(scoreboard));
     }
 
     /**
